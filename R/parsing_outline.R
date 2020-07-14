c88d0e77daffa18f6af072774cc7fff29b4273d3
Austin Marcus <bwvxnt@gmail.com>
2020-07-14 16:10:07 -0400

    parsing_outline.R for skeletoning psuedo code

---
+++ R/parsing_outline.R
@@ -0,0 +1,35 @@
+# psuedo code for data processing scheme
+
+rawfiles <- getAllFiles()
+partitionedFiles <- groupFilesByTaskSub(rawfiles, yamlMapping)
+# above step produces a list of tuples. the first dimension is task&subject pair. 2nd dimension is modality
+taskSubModeDfs <- mapply(read_data, yamlMapping) # output of wrangle step
+taskSubModeDfsQA <- qualityChecks(taskSubDfs) # output of quality checks step
+taskSubDf <- synthesize(taskSubDfsQA) # output of synthesis step
+
+read_behav <- function(filesOfTaskSub, parsers) {
+	taskSubRawDfComb <- sapply(filesOfTaskSub, getCorrectParser, parsers=parsers)
+	return(taskSubRawDfComb)
+}
+
+# input: takes all files for a given task and subject (so all modalities)
+# output: a data frame for each modality for that given task and subject
+	# does so by parsing each type of file, and combining them into a single data frame with complete information for all phases
+# parser mapping is known through the GROUP's NAME/class and via yaml file. tells which function to use for which FILE type
+read_data <- function(taskSubModeGroups, parserMap) {
+	taskSubBehDfs <- mapply(read_behav,
+						taskSubModeGroups$behav,
+						makeParserList(parserMap, taskSubModeGroups))
+	# ^^ do for each modality
+
+	# return flat vector of data frames for a given task-subject-modality
+	return(c(taskSubBehDfs, ...))
+}
+
+# input: a list of all the files in the experiment
+# output: a list of these files grouped by task and subject
+	# i.e. a single group has all the files associated with a given task and subject
+# TODO: how does it know how to group? where is this information obtained? yaml file to describe mapping?
+# TODO: parse filename information? should it expect "clean" s3 filename format outlined on slite?
+# TODO: should it attach information as to which parser to use on it here?
+groupFilesByTaskSub <- function
