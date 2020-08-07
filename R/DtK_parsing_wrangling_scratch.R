ca273f0df021e87add5cd6215c18af2cd8f69650
Sophie Paolizzi <sophie_paolizzi@Sophies-MacBook-Pro.local>
2020-08-07 11:12:59 -0400

    important wrangling functions

--- R/DtK_parsing_wrangling_scratch.R
+++ R/DtK_parsing_wrangling_scratch.R
@@ -2,41 +2,69 @@
 
 DtK <- data.table::fread("~/github_repos/experiment.pipeline/inst/examples/070_dtk_behav.csv")
 
-DtK_Ins <- DtK %>% dplyr::select(MinName, MinSound, block, corrAns, incorrAns, 
-                         Attacker.started, image_14.started, Fix2.started, fixation.started, 
-                         participant, `sequence #`,group, `participant initials`, date) %>% 
-  dplyr::filter(grepl("Ins",block)) %>% dplyr::select(-c(block)) %>%
-  rename(Trial_Fix = image_14.started)
+#Metadata
+Ins_metadata <- DtK %>% dplyr::select(MinName, block, corrAns, incorrAns, participant, 
+                                      `sequence #`,group, text_15.started, `participant initials`, date) %>%
+  rename(Switch_time = text_15.started) %>%
+  fill(Switch_time, .direction = "downup") %>%
+  dplyr::filter(grepl("Instr",block)) %>% dplyr::select(-c(block)) %>% unique()
 
 
+DtK_Ins <- DtK %>% dplyr::select(Attacker.started, Fix2.started, fixation.started, block) %>% 
+  dplyr::filter(grepl("Ins",block)) #%>% dplyr::select(-c(block)) 
 DtK_Ins <- DtK_Ins%>% 
   mutate(Fix_comb = paste0(DtK_Ins$fixation.started, Fix2.started)) %>% 
-  mutate(Event_time = paste0(DtK_Ins$Attacker.started, DtK_Ins$Fix_comb)) %>%
+  mutate(Event_time = paste0(Attacker.started, Fix_comb)) %>%
   mutate(attack_type = ifelse(Attacker.started != "NA", "Attack", "Avoid")) 
   
 DtK_Ins <- DtK_Ins %>% mutate(Event_type = ifelse(Fix_comb != "NANA", "Avoid", "Attack")) %>%
   #mutate(Event_type = paste0(DtK_Ins$attack_type, DtK_Ins$fix_type)) %>%
-  mutate(Event_time= sub("NA", "", Event_time)) %>% 
-  mutate(Event_time= sub("NA", "", Event_time)) %>% ##happens twice on purpose
+  mutate(Event_time= sub("NA", "", Event_time)) %>%
+  mutate(Event_time= sub("NA", "", Event_time)) %>%
+  ##happens twice on purpose
   mutate(Event_type = sub("NA", "", Event_type)) %>% 
   dplyr::select(-c(Fix_comb, attack_type, fixation.started, Fix2.started, Attacker.started)) %>% 
-  mutate(Event_time = as.numeric(Event_time))
+  mutate(Event_time = as.numeric(Event_time)) %>% na.omit()
 
 ###Figuring out log for keypresses
 
 log_clean_ins <- rbind(log_trials, log_data)
-log_clean_ins$Timestamp <- as.numeric(log_clean_ins$Timestamp)
-log_clean_ins <- log_clean_ins %>% arrange(Timestamp)
-log_clean_ins <- log_clean_ins %>% separate(Event, into = c("Event_type","Outcome"), sep = ":", extra = "merge")
-log_clean_ins <- log_clean_ins %>% dplyr::select(Timestamp, Event_type, Outcome)
-Keypress <- log_clean_ins %>% dplyr::filter(grepl("Keypress",Event_type)) 
-Keypress <- Keypress %>% rename(Keypress = Outcome) %>% mutate(Event_type = NULL) %>% 
-  dplyr::filter(Timestamp <= max(DtK_Ins$Event_time, na.rm = TRUE)) %>% dplyr::filter(!grepl("space",Keypress))
+log_clean_ins$Event_time <- as.numeric(log_clean_ins$Event_time)
+log_clean_ins <- log_clean_ins %>% arrange(Event_time)
+log_clean_ins <- log_clean_ins %>% separate(Event, into = c("string","Event_type"), sep = ":", extra = "merge")
+log_clean_ins <- log_clean_ins %>% dplyr::select(Event_time, string, Event_type)
+Ins_Keypress <- log_clean_ins %>% dplyr::filter(grepl("Keypress",string)) %>% 
+  #rename(Keypress = Event_type) %>% 
+  mutate(string = NULL) %>% 
+  dplyr::filter(Event_time <= max(DtK_Ins$Event_time, na.rm = TRUE)) %>% 
+  dplyr::filter(!grepl("space",Event_type)) %>% 
+  mutate(block = ifelse(Event_time >= Ins_metadata$Switch_time[1], "Instr_2", "Instr"))
+Ins_Trial_Fix <- DtK %>% dplyr::select(image_14.started) %>% 
+  rename(Trial_Fix = image_14.started) %>% 
+  mutate(Event_time = as.numeric(Trial_Fix)) %>% na.omit() %>% 
+  mutate(Event_type = "Trial_Fix") %>% na.omit() %>% mutate(Trial_Fix = NULL) %>% 
+  mutate(block = ifelse(Event_time >= Ins_metadata$Switch_time[1], "Instr_2", "Instr"))
+
+test <- rbind(Ins_Keypress,Ins_Trial_Fix, DtK_Ins) %>% arrange(Event_time)
+
+total <- 
+
+
+
+
+
+
 
 DtK_ins_quiz <- DtK %>% 
   dplyr::select("corrAns","InsAssess.started",  
                 "InsAssess.response", "slider.started", 
-                "slider.response") %>% na.omit()
+                "slider.response") %>% na.omit() %>% 
+  rename(b_effective = InsAssess.response) %>%
+  rename(n_effective = slider.response) %>%
+  rename(bq_timing = InsAssess.started) %>%
+  rename(nq_timing = slider.started)
+  
+
 DtK_Pav <- DtK %>%  dplyr::select('MinName', "Color", "corRes", 
                                   "Pavlovian.thisN","image.started",
                                   "image_2.started") %>% na.omit() %>% 
@@ -64,6 +92,9 @@ DtK_PIT <- DtK %>% dplyr::select("PIT.thisN", "PIT_2.thisN",
   dplyr::select(-c(PIT.thisN, PIT_2.thisN,PIT_block_1, PIT_block_2)) %>%
   na.omit()
    
+
+
+
 
 # DtK <- DtK %>% dplyr::select(MinName, MinSound, `sequence #`, block, Color, participant,
 #                       group, `participant initials`, date, corrAns, incorrAns, 
