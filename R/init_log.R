5f8d98f621b49e841496c3ea097c54a8efd62dc0
Nate Hall <nate.hall329@gmail.com>
2020-10-31 13:18:24 -0400

    add tryCatch.ep function to allow for simple code execution and logging of outcomes

--- R/init_log.R
+++ R/init_log.R
@@ -1,32 +1,58 @@
-#' Initializes .elog file which will store eye-specific messages about how QA checks are going. 
-#' 
-#'  Initially, my thought is to create .blog and .plog files for behavior and physio respectively and eventually write a separate function (or set of functions) that searches through logs and documents how certain checks went in a more succinct format (e.g. a .csv with subjects on rows and stages of QA on columns). 
+#' Initializes .elog file which will store eye-specific messages about how QA checks are going.
+#'
+#'  Initially, my thought is to create .blog and .plog files for behavior and physio respectively and eventually write a separate function (or set of functions) that searches through logs and documents how certain checks went in a more succinct format (e.g. a .csv with subjects on rows and stages of QA on columns).
 #'  A report can then be generated to either verify folks that look fine or to draw attention to problematic data.
-#' 
+#'
 
 init_eyelog <- function(log_dir = NULL){
-  
+
   if(is.null(log_dir)){log_dir <- getwd(); message("Generating .elog file in current directory: ", getwd())} else{
     message("Generating .elog file in: ", log_dir)
   }
-  
-  
+
+
   fname <- sub("(.*\\/)([^.]+)(\\.[[:alnum:]]+$)", "\\2", file) # strips ascending path and replaces file extension
   log_fname <- file.path(log_dir, paste0(fname, ".elog"))
-  
+
   if(file.exists(log_fname)){
     message("elog already exists for: ", fname, ". Overwriting.") #can entertain other options, but this is fine for now.
   }
-  
+
   sink(log_fname) # open sink
-  
-  cat("---------------------------------------------\n---------------------------------------------\nexperiment.pipeline eye QA log for: ", file, 
+
+  cat("---------------------------------------------\n---------------------------------------------\nexperiment.pipeline eye QA log for: ", file,
       "\n\nProcessed on ")
   cat(as.character(Sys.time()))
-      cat("\n---------------------------------------------\n---------------------------------------------\nsessionInfo:\n\n")
+  cat("\n---------------------------------------------\n---------------------------------------------\nsessionInfo:\n\n")
   print(sessionInfo())
   cat("\n---------------------------------------------\n---------------------------------------------\n")
-      
-  # cat("hello")
-  # sink()
+
 }
+
+
+
+#' TryCatch function to run subroutines with ease and standardized output.
+#'
+#' @param code chunk of code for tryCatch to evaluate. As in the TC documentation, multiple lines of code should be contained within {}.
+#' @param describe_text string containing standardized information about the significance of the code being run. This will print as COMPLETE for successful execution and ERROR and WARNING is something undesirable happens, while allowing code execution to continue below.
+
+tryCatch.ep <- function(code, describe_text = NULL, handle = "all"){
+  if(handle == "all"){
+    o <- tryCatch(code,
+                  error = function(c) {
+                    cat(describe_text, " ERROR (", gsub("\\n", "", as.character(c)),")\n", sep = "")
+                    return(c)},
+                  warning = function(c) {
+                    cat(describe_text, " WARNING (", gsub("\\n", "", as.character(c)),")\n", sep = "")
+                    return(c)},
+                  message = function(c) cat(describe_text, " MESSAGE (", gsub("\\n", "", as.character(c)),")\n", sep = "")
+    )
+  } else {
+    message("selective condition handling not yet available")
+  }
+  # print complete if no error or warning
+  if(!any(c("error", "warning") %in% class(o))) {cat(describe_text, " COMPLETE\n", sep = "")}
+}
+
+
+
