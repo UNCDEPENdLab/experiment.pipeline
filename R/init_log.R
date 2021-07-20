25db218aab189758ffc0e7b3d11227e76905b50c
Nate Hall <nate.hall329@gmail.com>
2021-07-20 09:48:46 -0400

    documentation .elog and trycatch.ep

--- R/init_log.R
+++ R/init_log.R
@@ -1,10 +1,13 @@
-#' Initializes .elog file which will store eye-specific messages about how QA checks are going.
-#'
-#'  Initially, my thought is to create .blog and .plog files for behavior and physio respectively and eventually write a separate function (or set of functions) that searches through logs and documents how certain checks went in a more succinct format (e.g. a .csv with subjects on rows and stages of QA on columns).
+#' @title Initializes .elog
+#' @description .elog files uses \code{sink()} to create a .txt file which will store eye-specific messages about how QA checks are going.
+#' @param file Path to the .edf file to process.
+#' @param log_dir Path to directory to store .elog. If NULL (default) will write to current directory.
+#' @param prefix Optional prefix to append to .elog name. 
+#' @return Nested list with processing options pulled from .yml configuration file.
+#' @note Initially, my thought is to create .blog and .plog files for behavior and physio respectively and eventually write a separate function (or set of functions) that searches through logs and documents how certain checks went in a more succinct format (e.g. a .csv with subjects on rows and stages of QA on columns).
 #'  A report can then be generated to either verify folks that look fine or to draw attention to problematic data.
-#'
-
-init_eyelog <- function(log_dir = NULL, prefix = NULL, file){
+#' @author Nate Hall
+init_eyelog <- function(file, log_dir = NULL, prefix = NULL){
 
   if(is.null(log_dir)){log_dir <- getwd(); message("Generating .elog file in current directory: ", getwd())} else{
     message("Generating .elog file in: ", log_dir)
@@ -33,10 +36,10 @@ init_eyelog <- function(log_dir = NULL, prefix = NULL, file){
 
 
 
-#' TryCatch function to run subroutines with ease and standardized output.
-#'
-#' @param code chunk of code for tryCatch to evaluate. As in the TC documentation, multiple lines of code should be contained within {}.
-#' @param describe_text string containing standardized information about the significance of the code being run. This will print as COMPLETE for successful execution and ERROR and WARNING is something undesirable happens, while allowing code execution to continue below.
+#' @title TryCatch function to run subroutines with ease and standardized output.
+#' @description This tryCatch wrapper prints errors and warnings alongside information describing the process being executed. If code runs succesfully will print COMPLETE. These errors/warnings are ideally passed to a log file that documents sequential steps in a pipeline. 
+#' @param code Chunk of code for tryCatch to evaluate. As in the TC documentation, multiple lines of code should be contained within curly brackets[{}].
+#' @param describe_text String containing standardized information about the significance of the code being run. This will print as COMPLETE for successful execution and ERROR and WARNING is something undesirable happens, while allowing code execution to continue below.
 
 tryCatch.ep <- function(code, describe_text = NULL){
     o <- tryCatch(code,
@@ -56,30 +59,8 @@ tryCatch.ep <- function(code, describe_text = NULL){
 }
 
 
-#' Open log chunks (to be run at the top of every sub-function of read_process_eye)
-#' simply standardizes for later crawler scripts to recognize
+#' @description  Prints log chunks (to be run at the top of every sub-function of read_process_eye)
+#' @note This simply standardizes for later crawler scripts to recognize.
+#' @param text Text to print into chunk header.
 
 log_chunk_header <- function(text){cat("--------------\n",text,"\n--------------\n")}
-
-
-#' simple increment function for steps: https://stackoverflow.com/questions/5738831/r-plus-equals-and-plus-plus-equivalent-from-c-c-java-etc
-# inc <- function(x){eval.parent(substitute(x <- x + 1))}
-
-
-
-
-
-#' generate warning message version of stopifnot
-#'
-#' @importFrom R.utils egsub
-#'
-
-# this was a fail. for some reason the trycatch exports an error.
-
-# warnifnot <- function(cond){
-#   if(!cond){warning(cond, "is not TRUE")}
-# }
-#
-# warnifnot <- stopifnot
-# body(warnifnot) <- do.call(substitute, list(body(stopifnot),
-#                                             list(stop = quote(warning))))
