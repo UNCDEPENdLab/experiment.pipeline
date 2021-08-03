bccca30ca6ea0d6ee6297719d6f1671605792813
Nate Hall <nate.hall329@gmail.com>
2021-08-03 10:39:03 -0400

    reorganize parse_events after moving btw_ev to initialize and changing args in uber funct

--- R/ep.eye_parse_events.R
+++ R/ep.eye_parse_events.R
@@ -10,44 +10,37 @@
 
 
 ep.eye_parse_events <- function(ep.eye, 
-                                inherit_btw_ev = NULL,
-                                event_info = NULL, 
-                                prefix = NULL,
+                                extraction_method,
+                                extract_event_func_path,
+                                csv_path, 
+                                msg_seq,
                                 header = NULL) { 
+  # debug
+  ep.eye <- eye_init
+  extraction_method = config$definitions$eye$msg_parse$extraction_method
+  extract_event_func_path = config$definitions$eye$msg_parse$extract_event_func_path
+  csv_path = file.path(config$definitions$eye$msg_parse$csv_dir_path, paste0(config$definitions$eye$global$prefix, ".csv"))
+  msg_seq = config$definitions$eye$msg_parse$msg_seq
+  header = "4. Parse task events:"
+
   
   if (!"ep.eye" %in% class(ep.eye)) { stop("parse_config_eye expects a pre-initialized ep.eye object") }
 
   if(!is.null(header)) log_chunk_header(header)
 
-  ### 4.1 Extract important between-event messages if requested. N.B. 4.1.2 move_to_within still needs work!!!
-  dt <- "- 4.1 Extract important between-event messages:\n"
-  if(!is.null(inherit_btw_ev)){
-    ep.eye <-  ep.eye_handle_between_event_msgs(ep.eye, 
-                                                inherit_btw_ev, 
-                                                dt)
-  } else{
-    cat(paste0(dt, " SKIP\n"))
-  }
+  ### 4.1 Extract event info
+  ep.eye <- ep.eye_parse_event_info(ep.eye, 
+                                    extraction_method = extraction_method,
+                                    extract_event_func_path = extract_event_func_path,
+                                    csv_path = csv_path,
+                                    msg_seq = msg_seq,
+                                    dt = "- 4.1 Parsing event information:\n")
 
-  ### 4.2 Extract event info
-  dt <- "- 4.2 Parsing event information:\n"
-  if(!is.null(event_info)){
-    ep.eye <- ep.eye_parse_event_info(ep.eye, 
-                                 extraction_method = event_info$extraction_method,
-                                 extract_event_func_path = event_info$extract_event_func_path,
-                                 csv_path = event_info$csv_path,
-                                 prefix = prefix,
-                                 msg_seq = event_info$msg_seq,
-                                 dt = dt)
-  } else{
-    cat(paste0(dt, " SKIP\n"))
-  }
-
-  ### 4.3 Event sequences check
-  dt <- "- 4.3 Validate message sequence and ordering:\n"
-  if(!is.null(event_info$msg_seq)){
+  ### 4.2 Event sequences check
+  dt <- "- 4.2 Validate message sequence and ordering:\n"
+  if(!is.null(msg_seq)){
     ep.eye <- ep.eye_validate_msg_seq(ep.eye, 
-                                      msg_seq = event_info$msg_seq,
+                                      msg_seq = msg_seq,
                                       dt = dt)
   } else{
     cat(paste0(dt, " SKIP\n"))
